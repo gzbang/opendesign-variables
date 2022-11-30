84d5c93f15da9c8fa61c07170fb363662e5c3658
Bang <15622356989@163.com>
2022-12-01 00:54:13 +0800

    fix: 修改配置

---
+++ app/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import type { UserConfig } from 'vitepress';
+
+const config: UserConfig = {
+  title: 'opendesign-variables',
+  base: '/opendesign-variables/',
+  titleTemplate: false,
+};
+export default config;
